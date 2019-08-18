3452fe48a85cce5c5b24e09324ce6aa993e122f3
dsomin <54242172+dsomin@users.noreply.github.com>
2019-08-18 18:15:49 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,10 @@
-<?php
-
-?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Страница статуса</title>
+</head>
+<body>
+	Привет <?php echo $_POST['first_name']; ?><br>
+	Твой E-mail: <?php echo $_POST['email']; ?>
+</body>
+</html>
